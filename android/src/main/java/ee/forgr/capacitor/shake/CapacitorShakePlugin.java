b0551bbf4f2c80f7cc6e793ec855b4acca0b72c3
Martin Donadieu <martindonadieu@gmail.com>
2025-10-15 18:09:33 +0100

    fix: lint

--- android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
+++ android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
@@ -9,46 +9,37 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 import com.squareup.seismic.ShakeDetector;
 
 @CapacitorPlugin(name = "CapacitorShake")
-public class CapacitorShakePlugin
-  extends Plugin
-  implements ShakeDetector.Listener {
+public class CapacitorShakePlugin extends Plugin implements ShakeDetector.Listener {
 
-  @Override
-  public void load() {
-    super.load();
+    @Override
+    public void load() {
+        super.load();
 
-    SensorManager sensorManager = null;
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-      sensorManager = (SensorManager) this.bridge.getActivity()
-        .getSystemService(Context.SENSOR_SERVICE);
+        SensorManager sensorManager = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            sensorManager = (SensorManager) this.bridge.getActivity().getSystemService(Context.SENSOR_SERVICE);
 
-      if (sensorManager == null) {
-        Log.e(
-          "CapacitorShakePlugin",
-          "This device couldn't find SENSOR_SERVICE. Perhaps your device doesn't support it"
-        );
-        return;
-      }
-    } else {
-      Log.e(
-        "CapacitorShakePlugin",
-        "This device doesn't support the getSystemService. Minimal android version: 23"
-      );
-      return;
+            if (sensorManager == null) {
+                Log.e("CapacitorShakePlugin", "This device couldn't find SENSOR_SERVICE. Perhaps your device doesn't support it");
+                return;
+            }
+        } else {
+            Log.e("CapacitorShakePlugin", "This device doesn't support the getSystemService. Minimal android version: 23");
+            return;
+        }
+
+        try {
+            ShakeDetector sd = new ShakeDetector(this);
+            sd.start(sensorManager, SensorManager.SENSOR_DELAY_GAME);
+            Log.i("CapacitorShakePlugin", "ShakeDetector started");
+        } catch (Exception e) {
+            Log.e("CapacitorShakePlugin", "Failed to start the shakeDetector", e);
+        }
     }
 
-    try {
-      ShakeDetector sd = new ShakeDetector(this);
-      sd.start(sensorManager, SensorManager.SENSOR_DELAY_GAME);
-      Log.i("CapacitorShakePlugin", "ShakeDetector started");
-    } catch (Exception e) {
-      Log.e("CapacitorShakePlugin", "Failed to start the shakeDetector", e);
+    @Override
+    public void hearShake() {
+        JSObject ret = new JSObject();
+        notifyListeners("shake", ret);
     }
-  }
-
-  @Override
-  public void hearShake() {
-    JSObject ret = new JSObject();
-    notifyListeners("shake", ret);
-  }
 }
