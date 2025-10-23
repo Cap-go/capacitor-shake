88dec6e23cfff247104084e9d83b17ae950ab9c5
Martin Donadieu <martindonadieu@gmail.com>
2025-10-23 13:39:11 +0100

    fix: add missing imports for PluginCall and PluginMethod

--- android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
+++ android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
@@ -5,6 +5,8 @@ import android.hardware.SensorManager;
 import android.util.Log;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.squareup.seismic.ShakeDetector;
 
